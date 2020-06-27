d2cdf28b24d4551f7ad1dab2fa7bd2e59d64c07a
asiekmurek <aaaa@o2.pl>
2020-06-27 22:10:07 +0200

    ApolloClient return data in console

--- src/index.js
+++ src/index.js
@@ -2,12 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 import App from './App';
-import { ApolloClient } from "@apollo/client"
+import { ApolloClient, InMemoryCache, HttpLink, gql, ApolloProvider } from "@apollo/client"
 
+const URL = "https://agile-cliffs-85240.herokuapp.com/";
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: URL
+  })
+
+});
+
+const RANDOM_QUOTE_QUERY = gql`
+query getRandomQuote {
+  randomQuote {
+    text
+    author
+  }
+}
+`;
+client.query({ query: RANDOM_QUOTE_QUERY }).then(result => console.log(result.data))
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
